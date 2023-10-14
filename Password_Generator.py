c57ae276ff35c5eafb4e814dc875bd2598d48383
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2023-10-14 17:39:40 +0500

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,10 +1,13 @@
+# importing system libary
 from os import system
+# importing random Libary (for Random number,character and symbils generation)
 import random
 
 letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '[', ']', '{', '}', '|', '\\', ';', ':', ',', '.', '<', '>', '/', '?', '`', '~', "'", '"']
 
+# clear screen
 system('cls')
 print("Welcome to the Password Generator!")
 
@@ -28,4 +31,5 @@ password = ""
 for char in password_list:
     password += char
 
+# Final Result.
 print(f"Your Password is : {password}")
